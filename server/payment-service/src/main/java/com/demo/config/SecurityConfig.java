08e731e1dfdef7f6c36c35878b41a065abaa596e
aleksandr_matveev <aleksandr_matveev1@epam.com>
2022-11-06 15:05:46 +0300

    add library for openapi spec auto generation and swagger ui with bearer token

---
+++ server/payment-service/src/main/java/com/demo/config/SecurityConfig.java
@@ -0,0 +1,65 @@
+package com.demo.config;
+
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+@KeycloakConfiguration
+@ConditionalOnProperty(name = "security.config.use-keycloak", havingValue = "true")
+public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+
+    private static final String[] SWAGGER_WHITELIST = {
+            "/v3/api-docs/**",
+            "/swagger-ui/**",
+            "/swagger-ui.html",
+    };
+
+    public static void configureApiSecurity(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers(SWAGGER_WHITELIST)
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        configureApiSecurity(http);
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(getKeycloakAuthenticationProvider());
+    }
+
+    private KeycloakAuthenticationProvider getKeycloakAuthenticationProvider() {
+        KeycloakAuthenticationProvider authenticationProvider = keycloakAuthenticationProvider();
+        authenticationProvider.setGrantedAuthoritiesMapper(getAuthoritiesMapper());
+
+        return authenticationProvider;
+    }
+
+    private GrantedAuthoritiesMapper getAuthoritiesMapper() {
+        SimpleAuthorityMapper mapper = new SimpleAuthorityMapper();
+        mapper.setPrefix("");
+        return mapper;
+    }
+
+    @Bean
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+}
